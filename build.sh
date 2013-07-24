f6c550f0fad57b6b6dbc7a174e0e7d1e37f95ed0
Tom De Vylder <tom@penumbra.be>
2013-07-24 14:41:17 +0200

    Run standalone

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 set -e
 
-PKG_VERSION=$(grep -i version package.json | cut -d '"' -f4)
+rm -rf build
+mkdir -p build/usr/share
+git clone https://github.com/elasticsearch/kibana.git build/usr/share/kibana3
+rm -rf build/usr/share/kibana3/.git*
+
+PKG_VERSION=$(
+  grep -i version build/usr/share/kibana3/package.json \
+    | cut -d '"' -f4
+)
 
 if [ ! -f kibana3_${PKG_VERSION}_all.deb ]
 then
-  rm -rf build
-  mkdir -p build/usr/share
-  git clone . build/usr/share/kibana3
-  rm -rf build/usr/share/kibana3/.git*
 
   echo "#!/bin/sh" > build/post-install
   echo "chown -R www-data:www-data /usr/share/kibana3" >> build/post-install
